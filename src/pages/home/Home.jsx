7750e5201810c6b3ae43060cc10df8f32a6db249
DANI GUNANTO <111202113682@mhs.dinus.ac.id>
2024-04-18 08:48:48 +0700

    update latest

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-    <div className="widget">
-      <Widget type="user" />
+    <div className="widget"> 
+      <Widget type="user" /> 
       <Widget type="order" />
       <Widget type="earning" />
       <Widget type="balance" />
