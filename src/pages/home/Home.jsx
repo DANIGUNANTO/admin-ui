3c7b031b8e2104261e1ec55db149cdf8535c7fad
DANI GUNANTO <111202113682@mhs.dinus.ac.id>
2024-04-18 08:18:03 +0700

    ubah teks di app

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,28 @@
+import "./home.scss";
+import Sidebar from "../../component/sidebar/Sidebar";
+import Navbar from "../../component/navbar/Navbar";
+import Widget from "../../component/widget/Widget";
+import Chart from "../../component/chart/Chart";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+    <div className="widget">
+      <Widget type="user" />
+      <Widget type="order" />
+      <Widget type="earning" />
+      <Widget type="balance" />
+      </div>
+      <div className="charts">
+        <Chart type="order" aspect={2 / 1} />
+        <Chart type="earning" aspect={2 / 1} />
+      </div>
+    </div>
+  </div>  
+  );
+};
+
+export default Home;
